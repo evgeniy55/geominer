22dd9a42afc4c7c7941411d35aede0a65017e4c1
Evg55 <ackbar5@yandex.ru>
2025-06-20 15:37:04 +0600

    fix event

--- miner.lua
+++ miner.lua
@@ -1,5 +1,4 @@
---local component = require('component') -- подгрузить обертку из OpenOS
---local computer = require('computer')
+local event = require("event")
 local chunks = 9 -- количество чанков для добычи
 local min, max = 2.2, 40 -- минимальная и максимальная плотность
 local port = 1 -- порт для взаимодействия с роботом
